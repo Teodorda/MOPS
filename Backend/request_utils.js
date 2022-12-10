896cac783893485b772ff8ae793999540c2fc918
Marius Grafu <grafumarius@gmail.com>
2022-12-10 11:37:45 +0200

    task6 part2: add content moderation for requests

---
+++ Backend/request_utils.js
@@ -0,0 +1,124 @@
+const con = require("./db_con").con;
+const auth_utils = require("./auth_utils");
+
+const getRequests = (req, res) => {
+  con.query("SELECT * FROM cereri", function (err, result, fields) {
+    if (err) {
+      res.json({ products: "error" });
+    }
+
+    res.json({ products: result });
+  });
+};
+
+const deleteRequest = async (req, res) => {
+  const id = req.body.id;
+  const userJwt = req.body.userJwt;
+  const user_id = auth_utils.getUserIdFromJwt(userJwt);
+
+  if (await auth_utils.canUserEditEntity(user_id, id, getRequestById)) {
+    con.query(
+      "DELETE FROM cereri WHERE id=" + id,
+      function (err) {
+        if (err) {
+          res.json({ delete: "false" });
+        }
+        res.json({ delete: "true" });
+      }
+    );
+  } else {
+    res.json({ delete: "false" });
+  }
+};
+
+const postRequest = (req, res) => {
+  const nume = req.body.nume;
+  const cantitate = req.body.cantitate;
+  const adresa = req.body.adresa;
+  const d = new Date();
+  let month = d.getMonth() + 1;
+  let year = d.getFullYear();
+  const userJwt = req.body.userJwt;
+  const user_id = auth_utils.getUserIdFromJwt(userJwt);
+
+  if (!user_id) {
+    res.json({ add: "false" });
+    return;
+  }
+
+  con.query(
+    "INSERT INTO cereri (nume, cantitate, adresa, luna, an, user_id) value ('" +
+      nume +
+      "', '" +
+      cantitate +
+      "', '" +
+      adresa +
+      "', '" +
+      month +
+      "', '" +
+      year +
+      "', '" +
+      user_id +
+      "')",
+    function (err) {
+      if (err) {
+        res.json({ add: "false" });
+      }
+      res.json({ add: "true" });
+    }
+  );
+};
+
+const putRequest = async (req, res) => {
+  const id = req.body.id;
+  const nume = req.body.nume;
+  const cantitate = req.body.cantitate;
+  const adresa = req.body.adresa;
+  const userJwt = req.body.userJwt;
+  const user_id = auth_utils.getUserIdFromJwt(userJwt);
+
+  if (await auth_utils.canUserEditEntity(user_id, id, getRequestById)) {
+    console.log("CAN EDIT");
+  con.query(
+    "UPDATE cereri SET nume='" +
+      nume +
+      "', cantitate='" +
+      cantitate +
+      "', adresa='" +
+      adresa +
+      "' WHERE id=" +
+      id +
+      "",
+    function (err) {
+      if (err) {
+        res.json({ modify: "false" });
+      }
+      res.json({ modify: "true" });
+    }
+  );
+} else {
+    res.json({ modify: "false" });
+  }
+};
+
+const getRequestById = (requestId) => {
+  return new Promise((resolve) => {
+    con.query(
+      `SELECT * FROM cereri WHERE id=${requestId}`,
+      function (err, result) {
+        if (err) {
+          resolve(null);
+        }
+
+        resolve(result[0]);
+      }
+    );
+  });
+};
+
+module.exports = {
+  postRequest,
+  deleteRequest,
+  getRequests,
+  putRequest,
+};
--- Backend/transaction_utils.js
+++ Backend/transaction_utils.js
@@ -1,190 +1,102 @@
-const con = require("./db_con").con
+const con = require("./db_con").con;
 
 const postHistoryDonator = (req, res) => {
-    const id_product = req.body.id_product
-    const name = req.body.name
-    const cantitate = req.body.cantitate
-    const donator = req.body.donator
-    const user_id = req.body.user_id
+  const id_product = req.body.id_product;
+  const name = req.body.name;
+  const cantitate = req.body.cantitate;
+  const donator = req.body.donator;
+  const user_id = req.body.user_id;
 
-    con.query(
-        "INSERT INTO history_donator (name, cantitate, donator, user_id) value ('" +
-        name +
-        "', '" +
-        cantitate +
-        "', '" +
-        donator +
-        "', '" +
-        user_id +
-        "')",
-        function (err) {
-            if (err) {
-                console.log(err)
-            }
-        }
-    )
+  con.query(
+    "INSERT INTO history_donator (name, cantitate, donator, user_id) value ('" +
+      name +
+      "', '" +
+      cantitate +
+      "', '" +
+      donator +
+      "', '" +
+      user_id +
+      "')",
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 
-    con.query("DELETE FROM products WHERE id=" + id_product,
-        function (err) {
-            if (err) {
-                console.log(err)
-            }
-            res.json({ transfer: 'true' })
-        }
-    )
-}
-
-
-const getHistoryDonator = (req, res) => {
-
-    const user_id = req.body.user_id
-
-    con.query(
-        "SELECT * FROM history_donator WHERE user_id='" + user_id + "'",
-        function (err, result, fields) {
-            if (err) {
-                res.json({ products: 'error' })
-            }
-
-            res.json({ history: result })
-
-        })
-}
-
-const postHistoryBeneficiary = (req, res) => {
-    const id_cerere = req.body.id_cerere
-    const name = req.body.name
-    const cantitate = req.body.cantitate
-    const beneficiar = req.body.beneficiar
-    const user_id = req.body.user_id
-
-    con.query(
-        "INSERT INTO history_beneficiar (name, cantitate, beneficiar, user_id) value ('" +
-        name +
-        "', '" +
-        cantitate +
-        "', '" +
-        beneficiar +
-        "', '" +
-        user_id +
-        "')",
-        function (err) {
-            if (err) {
-                console.log(err)
-            }
-        }
-    )
-
-    con.query("DELETE FROM cereri WHERE id=" + id_cerere,
-        function (err) {
-            if (err) {
-                console.log(err)
-            }
-            res.json({ transfer: 'true' })
-        }
-    )
-}
-
- const getHistoryBeneficiary = (req, res) => {
-
-    const user_id = req.body.user_id
-
-    con.query(
-        "SELECT * FROM history_beneficiar WHERE user_id='" + user_id + "'",
-        function (err, result, fields) {
-            if (err) {
-                res.json({ products: 'error' })
-            }
-
-            res.json({ history: result })
-
-        })
-}
-
-const getRequest = (req, res) => {
-    con.query(
-        "SELECT * FROM cereri",
-        function (err, result, fields) {
-            if (err) {
-                res.json({ products: 'error' })
-            }
-
-            res.json({ products: result})
-            
-    })
+  con.query("DELETE FROM products WHERE id=" + id_product, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.json({ transfer: "true" });
+  });
 };
 
-const deleteRequest = (req, res) => {
-    const id = req.body.id
+const getHistoryDonator = (req, res) => {
+  const user_id = req.body.user_id;
 
-    con.query(
-        "DELETE FROM cereri WHERE id="+id,
-        function (err, result, fields) {
-        if (err) {
-            res.json({ delete: 'false' })
-        }
-        res.json({ delete: 'true' })
-        }
-    )
-}
+  con.query(
+    "SELECT * FROM history_donator WHERE user_id='" + user_id + "'",
+    function (err, result, fields) {
+      if (err) {
+        res.json({ products: "error" });
+      }
 
-const addRequest = (req, res) => {
-    const nume = req.body.nume
-    const cantitate = req.body.cantitate
-    const adresa = req.body.adresa
-    const d = new Date();
-    let month = d.getMonth()+1;
-    let year = d.getFullYear();
-    const user_id = req.body.user_id
+      res.json({ history: result });
+    }
+  );
+};
 
-    con.query(
-        "INSERT INTO cereri (nume, cantitate, adresa, luna, an, user_id) value ('" +
-        nume +
-        "', '" +
-        cantitate +
-        "', '" +
-        adresa +
-        "', '" +
-        month +
-        "', '" +
-        year + 
-        "', '" +
-        user_id +
-        "')",
-        function (err, result, fields) {
-        if (err) {
-            res.json({ add: 'false' })
-        }
-        res.json({ add: 'true' })
-        }
-    )
-}
+const postHistoryBeneficiary = (req, res) => {
+  const id_cerere = req.body.id_cerere;
+  const name = req.body.name;
+  const cantitate = req.body.cantitate;
+  const beneficiar = req.body.beneficiar;
+  const user_id = req.body.user_id;
 
-const modifyRequest = (req, res) => {
-    const id = req.body.id
-    const nume = req.body.nume
-    const cantitate = req.body.cantitate
-    const adresa = req.body.adresa
+  con.query(
+    "INSERT INTO history_beneficiar (name, cantitate, beneficiar, user_id) value ('" +
+      name +
+      "', '" +
+      cantitate +
+      "', '" +
+      beneficiar +
+      "', '" +
+      user_id +
+      "')",
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 
-    con.query(
-        "UPDATE cereri SET nume='"+nume+"', cantitate='"+cantitate+"', adresa='"+adresa+"' WHERE id="+id+"",
-        function (err, result, fields) {
-        if (err) {
-            res.json({ modify: 'false' })
-        }
-        res.json({ modify: 'true' })
-        }
-    )
-}
+  con.query("DELETE FROM cereri WHERE id=" + id_cerere, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.json({ transfer: "true" });
+  });
+};
+
+const getHistoryBeneficiary = (req, res) => {
+  const user_id = req.body.user_id;
+
+  con.query(
+    "SELECT * FROM history_beneficiar WHERE user_id='" + user_id + "'",
+    function (err, result, fields) {
+      if (err) {
+        res.json({ products: "error" });
+      }
+
+      res.json({ history: result });
+    }
+  );
+};
 
 module.exports = {
-    postHistoryDonator,
-    getHistoryDonator,
-    postHistoryBeneficiary,
-    getHistoryBeneficiary,
-    addRequest,
-    deleteRequest,
-    getRequest,
-    modifyRequest
-}
-
+  postHistoryDonator,
+  getHistoryDonator,
+  postHistoryBeneficiary,
+  getHistoryBeneficiary,
+};
